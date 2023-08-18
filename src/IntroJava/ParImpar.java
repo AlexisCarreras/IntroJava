843d9e75937229a51571669a4ffa1d5d1a29a4e6
Alexis Carreras <acarreras@cdainfo.com>
2023-08-18 17:03:40 -0300

    chore: ej 7

---
+++ src/IntroJava/ParImpar.java
@@ -0,0 +1,20 @@
+package IntroJava;
+
+public class ParImpar {
+
+	public static void parImpar(int num) {
+		if (num > 0) {
+			if (num % 2 == 0) {
+				System.out.println("Par");
+			} else {
+				System.out.println("Impar");
+			}
+		} else {
+			System.out.println("El número ingresado debe ser mayor a 0.");
+		}
+	}
+
+	public static void main(String[] args) {
+		parImpar(0);
+	}
+}
